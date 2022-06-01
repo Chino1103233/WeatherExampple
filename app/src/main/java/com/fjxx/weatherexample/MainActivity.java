ec5e0f53db8507f2ac6a001ec0df140f552ffaf4
Chino1103233 <wumxing@live.com>
2022-06-01 19:24:55 +0800

    First commit.

---
+++ app/src/main/java/com/fjxx/weatherexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fjxx.weatherexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
